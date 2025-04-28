f8004043d9ff5f540562783345132b04a76a202e
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:15:46 -0300

    Update 2025_04_20_183605_paciente.php

--- 2025_04_20_183605_paciente.php
+++ 2025_04_20_183605_paciente.php
@@ -6,25 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('pacientes', function (Blueprint $table) {
             $table->id();
-            $table->string('nome');
+            $table->string('nome'); 
             $table->string('telefone')->nullable();
             $table->string('endereco')->nullable();
             $table->date('data_nascimento')->nullable();
-            $table->string('email')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('pacientes');
