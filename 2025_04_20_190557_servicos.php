85f01760e81f17545c19fc51aedff469a3dc9e8e
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 14:45:01 -0300

    Update and rename 2025_04_20_190557_sabor.php to 2025_04_20_190557_servicos.php

--- 2025_04_20_190557_servicos.php
+++ 2025_04_20_190557_servicos.php
@@ -8,16 +8,17 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('sabor', function (Blueprint $table) {
+        Schema::create('servicos', function (Blueprint $table) {
             $table->id(); // chave primária padrão
             $table->string('nome');
-            $table->string('tipo');
+            $table->string('tipo'); // Ex: Exame, Procedimento, Consulta Especial
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('sabor');
+        Schema::dropIfExists('servicos');
     }
 };
+
