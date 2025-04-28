49e02568c813615c18d4edd52fd7ae5603d4f7b9
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 15:35:03 -0300

    Update and rename clienteEdit.blade.php to pacienteEdit.blade.php

--- pacienteEdit.blade.php
+++ pacienteEdit.blade.php
@@ -2,30 +2,30 @@
 
 @section('content')
 <div class="container">
-    <h2>Editar Cliente</h2>
+    <h2>Editar Paciente</h2>
 
-    <form action="{{ route('cliente.update', $cliente->id) }}" method="POST">
+    <form action="{{ route('paciente.update', $paciente->id) }}" method="POST">
         @csrf
         @method('PUT')
 
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
-            <input type="text" name="nome" id="nome" class="form-control" value="{{ $cliente->nome }}" required>
+            <input type="text" name="nome" id="nome" class="form-control" value="{{ $paciente->nome }}" required>
         </div>
 
         <div class="mb-3">
             <label for="telefone" class="form-label">Endereço:</label>
-            <input type="text" name="telefone" id="endereco" class="form-control" value="{{ $cliente->endereco }}">
+            <input type="text" name="telefone" id="endereco" class="form-control" value="{{ $paciente->endereco }}">
         </div>
 
         
         <div class="mb-3">
             <label for="telefone" class="form-label">Telefone:</label>
-            <input type="text" name="telefone" id="telefone" class="form-control"value="{{ $cliente->telefone }}">
+            <input type="text" name="telefone" id="telefone" class="form-control"value="{{ $paciente->telefone }}">
         </div>
 
         <button type="submit" class="btn btn-primary">Atualizar</button>
-        <a href="{{ route('cliente.index') }}" class="btn btn-secondary">Cancelar</a>
+        <a href="{{ route('paciente.index') }}" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
 @endsection
