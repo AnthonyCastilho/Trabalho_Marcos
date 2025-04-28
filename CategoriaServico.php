4e740d554ffaa95c9f1a29659dd56328fb08882c
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:27:06 -0300

    Update CategoriaServico.php

--- CategoriaServico.php
+++ CategoriaServico.php
@@ -7,13 +7,8 @@ class CategoriaServico extends Model
 {
     use HasFactory;
 
-    // Se você não precisar de timestamps, adicione: 
-    // public $timestamps = false;
-
     protected $fillable = ['descricao'];
 
-    // Exemplo de relacionamento de um-para-muitos com o modelo Servico
-    public function servicos()
     {
         return $this->hasMany(Servico::class);
     }
