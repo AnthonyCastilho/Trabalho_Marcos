66896be64e3e043fe3c11bffda5d801008f9bf49
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:23:11 -0300

    Update 2025_04_20_230120_create_itens_agendamento_table.php

--- 2025_04_20_230120_create_itens_agendamento_table.php
+++ 2025_04_20_230120_create_itens_agendamento_table.php
@@ -8,19 +8,23 @@ class CreateItensAgendamentoTable extends Migration
 {
     public function up()
     {
-        Schema::create('itens_agendamento', function (Blueprint $table) {
+        Schema::create('itens_agendamentos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('agendamento_id')->constrained('agendamentos')->onDelete('cascade');
             $table->foreignId('especialidade_id')->constrained('especialidades')->onDelete('restrict');
             $table->foreignId('plano_id')->constrained('planos')->onDelete('restrict');
-            $table->integer('quantidade')->default(1); // Quantidade de consultas ou procedimentos
+            $table->integer('quantidade')->default(1);
             $table->timestamps();
+            
+            $table->index('agendamento_id');
+            $table->index('especialidade_id');
+            $table->index('plano_id');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('itens_agendamento');
+        Schema::dropIfExists('itens_agendamentos');
     }
 }
 
