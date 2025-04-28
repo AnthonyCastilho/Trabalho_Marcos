0637d66a37f24290e3487f1b6b0fb7ed7d2c4eb9
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 15:57:32 -0300

    Update and rename createPizza.blade.php to createServico.blade.php

--- createServico.blade.php
+++ createServico.blade.php
@@ -1,17 +1,25 @@
 @extends('layout')
 
 @section('content')
-    <h2>Nova Pizza</h2>
-    <form action="{{ route('pizza.store') }}" method="POST">
+    <h2>Novo Serviço</h2>
+    <form action="{{ route('servico.store') }}" method="POST">
         @csrf
         <div class="form-group">
-            <label for="nome">Nome da Pizza</label>
+            <label for="nome">Nome do Serviço</label>
             <input type="text" name="nome" id="nome" class="form-control" required>
         </div>
+        
         <div class="form-group">
-            <label for="nome">Preço da Pizza</label>
+            <label for="descricao">Descrição</label>
+            <textarea name="descricao" id="descricao" class="form-control" rows="3"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="preco_base">Preço Base</label>
             <input type="text" name="preco_base" id="preco_base" class="form-control" required>
         </div>
+
         <button type="submit" class="btn btn-primary mt-3">Salvar</button>
     </form>
 @endsection
+
