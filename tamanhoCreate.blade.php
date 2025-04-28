268b2bb7e42d2c4f113c0da323053f5179acbbf2
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:47:32 -0300

    Update clinicaCreate.blade.php

--- tamanhoCreate.blade.php
+++ tamanhoCreate.blade.php
@@ -1,17 +1,18 @@
 @extends('layout')
 
 @section('content')
-    <h2>Nova Descrição de Tamanho</h2>
-    <form action="{{ route('tamanho.store') }}" method="POST">
+    <h2>Nova Categoria de Serviço</h2>
+    <form action="{{ route('categoria-servico.store') }}" method="POST">
         @csrf
         <div class="form-group">
-            <label for="descricao">Descrição do Tamanho</label>
+            <label for="descricao">Descrição da Categoria</label>
             <input type="text" name="descricao" id="descricao" class="form-control" required>
         </div>
         <div class="form-group">
-            <label for="descricao">Preço Extra</label>
-            <input type="text" name="preco_extra" id="preco_extra" class="form-control" required>
+            <label for="valor_extra">Valor Extra</label>
+            <input type="number" step="0.01" name="valor_extra" id="valor_extra" class="form-control" required>
         </div>
         <button type="submit" class="btn btn-primary mt-3">Salvar</button>
     </form>
 @endsection
+
