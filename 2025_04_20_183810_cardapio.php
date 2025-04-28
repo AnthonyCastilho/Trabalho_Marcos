2ca9faee1d9d2e60362c5e94ad6678619504ed6a
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 14:19:36 -0300

    Update 2025_04_20_183810_cardapio.php

--- 2025_04_20_183810_cardapio.php
+++ 2025_04_20_183810_cardapio.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pizzas', function (Blueprint $table) {
+        Schema::create('lanches', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->text('descricao')->nullable();
