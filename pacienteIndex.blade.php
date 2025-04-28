df910c791b619e3b34878a0721a426b9d23d2662
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:01:53 -0300

    Update pacienteIndex.blade.php

--- pacienteIndex.blade.php
+++ pacienteIndex.blade.php
@@ -6,34 +6,34 @@
 
     <a href="{{ route('paciente.create') }}" class="btn btn-success mb-3">Novo Paciente</a>
 
-
-        <table class="table table-bordered table-striped">
-            <thead>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($paciente_tb as $paciente)
                 <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Telefone</th>
-                    <th>Ações</th>
+                    <td>{{ $paciente->id }}</td>
+                    <td>{{ $paciente->nome }}</td>
+                    <td>{{ $paciente->telefone }}</td>
+                    <td>
+                        <a href="{{ route('paciente.edit', $paciente->id) }}" class="btn btn-warning btn-sm">Editar</a>
+                        <form action="{{ route('paciente.destroy', $paciente->id) }}" method="POST" class="d-inline"
+                              onsubmit="return confirm('Tem certeza que deseja excluir este paciente?');">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm">Excluir</button>
+                        </form>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($paciente_tb as $paciente)
-                    <tr>
-                        <td>{{ $paciente->id }}</td>
-                        <td>{{ $paciente->nome }}</td>
-                        <td>{{ $paciente->telefone }}</td>
-                        <td>
-                            <a href="{{ route('paciente.edit', $paciente->id) }}" class="btn btn-warning btn-sm">Editar</a>
-                            <form action="{{ route('paciente.destroy', $paciente->id) }}" method="POST" class="d-inline"
-                                  onsubmit="return confirm('Tem certeza que deseja excluir este paciente?');">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger btn-sm">Excluir</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
+
