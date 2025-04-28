6044711487d7c37f63e55f4ed7916c7020cb1541
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:00:06 -0300

    Update layout.blade.php

--- layout.blade.php
+++ layout.blade.php
@@ -3,17 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizzaria - Sistema de Pedidos</title>
+    <title>Clínica - Sistema de Agendamentos</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/darkly/bootstrap.min.css" rel="stylesheet">
-
 </head>
 <body>
 
     <!-- Barra de navegação -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">Pizzaria</a>
+            <a class="navbar-brand" href="{{ url('/') }}">Clínica</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -21,36 +20,36 @@
             
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <!-- Pedidos -->
+                    <!-- Agendamentos -->
                     <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('pedidos.index') }}">Pedidos</a>
+                        <a class="nav-link active" href="{{ route('agendamento.index') }}">Agendamentos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('pedidos.create') }}">Novo Pedido</a>
-                    </li>
-
-                    <!-- Clientes -->
-                    <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('cliente.index') }}">Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cliente.create') }}">Novo Cliente</a>
+                        <a class="nav-link" href="{{ route('agendamento.create') }}">Novo Agendamento</a>
                     </li>
 
-                    <!-- Pizzas -->
+                    <!-- Pacientes -->
                     <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('pizza.index') }}">Pizzas</a>
+                        <a class="nav-link active" href="{{ route('paciente.index') }}">Pacientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('pizza.create') }}">Nova Pizza</a>
+                        <a class="nav-link" href="{{ route('paciente.create') }}">Novo Paciente</a>
                     </li>
 
-                    <!-- Tamanhos -->
+                    <!-- Serviços -->
                     <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('tamanho.index') }}">Tamanhos</a>
+                        <a class="nav-link active" href="{{ route('servico.index') }}">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tamanho.create') }}">Novo Tamanho</a>
+                        <a class="nav-link" href="{{ route('servico.create') }}">Novo Serviço</a>
+                    </li>
+
+                    <!-- Categorias de Serviço (se precisar) -->
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('categoria.index') }}">Categorias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('categoria.create') }}">Nova Categoria</a>
                     </li>
                 </ul>
             </div>
