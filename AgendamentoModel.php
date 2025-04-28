5d42dee5ab4e1edf9bbb25b40b25b83a3db72eaa
AnthonyCastilho <anthonyhcastilho@gmail.com>
2025-04-28 16:23:55 -0300

    Update AgendamentoModel.php

--- AgendamentoModel.php
+++ AgendamentoModel.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Agendamento extends Model
 {
-    protected $table = 'agendamentos'; // Nome da tabela no banco
+    protected $table = 'agendamentos';
 
     protected $fillable = ['cliente_id', 'servico_id', 'data_agendamento', 'horario', 'status'];
+
+    protected $dates = ['data_agendamento'];
 
     public function cliente()
     {
@@ -18,5 +20,15 @@ class Agendamento extends Model
     public function servico()
     {
         return $this->belongsTo(Servico::class, 'servico_id');
+    }
+
+    public function getHorarioAttribute($value)
+    {
+        return \Carbon\Carbon::parse($value)->format('H:i');
+    }
+
+    public function setHorarioAttribute($value)
+    {
+        $this->attributes['horario'] = \Carbon\Carbon::parse($value)->format('H:i');
     }
 }
